7f799c821a54c2b07d7d8de68348a8f150e7ee46
luochana <1329127421@qq.com>
2019-07-10 00:06:46 +0800

    add spark_news

---
+++ spark_news/src/main/scala/com/luochan/utils/DBLocalUtils.scala
@@ -0,0 +1,30 @@
+package com.luochen.utils
+
+import java.sql.{Connection, DriverManager}
+;
+
+object DBLocalUtils {
+  val url = "jdbc:mysql://localhost:3306/mydatabase?useUnicode=true&characterEncoding=utf-8"
+  val username = "root"
+  val password = "luochen1998"
+
+  classOf[com.mysql.jdbc.Driver]
+
+  def getConnection(): Connection = {
+    DriverManager.getConnection(url, username, password)
+  }
+
+  def close(conn: Connection): Unit = {
+    try{
+      if(!conn.isClosed() || conn != null){
+        conn.close()
+      }
+    }
+    catch {
+      case ex: Exception => {
+        ex.printStackTrace()
+      }
+    }
+  }
+
+}
