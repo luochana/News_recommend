c039e6a71b5951a950357d1799ce0d8acf226b46
luochana <1329127421@qq.com>
2019-07-19 01:51:36 +0800

    add something new

---
+++ NewsRecommendSystem_web/src/main/java/com/luochen/web/Dao/interf/ItemSimRepository.java
@@ -0,0 +1,7 @@
+package com.luochen.web.Dao.interf;
+
+import com.luochen.web.Dao.ItemSim;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ItemSimRepository extends CrudRepository<ItemSim,Long> {
+}
