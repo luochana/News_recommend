255f2c7f9fd2a798ade7b38ab2487dd8dd43d77e
luochana <1329127421@qq.com>
2021-01-20 01:56:25 +0800

    add something new

---
+++ flink_news/src/main/scala/HotNewsStat.scala
@@ -0,0 +1,28 @@
+import java.util.Properties
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+import org.apache.flink.streaming.api.scala._
+
+
+object HotNewsStat {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+
+    val properties = new Properties()
+    properties.setProperty("bootstrap.servers", "spark2:9092")
+    properties.setProperty("key.deserializer",
+      "org.apache.kafka.common.serialization.StringDeserializer")
+    properties.setProperty("value.deserializer",
+      "org.apache.kafka.common.serialization.StringDeserializer")
+
+    val inputStream = env.addSource( new FlinkKafkaConsumer[String]("first", new SimpleStringSchema(), properties) )
+
+
+  }
+
+}
